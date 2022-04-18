7711a312ed7b07d030a6886198ac1f888ab8c13d
Bhavdeep <58917780+bhavdeep-saragadam@users.noreply.github.com>
2022-04-18 15:09:21 -0400

    Hopefull

--- global/server.js
+++ global/server.js
@@ -1,12 +1,16 @@
-
 const io = require('socket.io')(3000)
 
 const users = {}
+
+socket.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
+    console.log('user connected')
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
